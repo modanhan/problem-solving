4114513bd211039f0cc14bb3f58a705843f30865
Modan Han <modanhan@live.com>
2015-03-11 14:08:36 -0600

    easier implementation for easy problem

--- psc/ccpc 2015/div2a.cpp
+++ psc/ccpc 2015/div2a.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -7,24 +6,24 @@ int main(){
 	int t; cin >> t;
 	string a;
 	getline(cin, a);
-	char chars[] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+	string vowels="aeiouAEIOU";
 	while (t--){
 		string l;
 		getline(cin, l);
-		int vow = 0, con = 0;
+		int vnum = 0, cnum = 0;
 		for (char c : l){
 			bool v = 0;
-			for (char x : chars){
+			for (char x : vowels){
 				if (x == c){
-					vow++;
+					vnum++;
 					v = 1;
 					break;
 				}
 			}
 			if (!v&&c!=' '){
-				con++;
+				cnum++;
 			}
 		}
-		cout << con << ' ' << vow << '\n';
+		cout << cnum << ' ' << vnum << '\n';
 	}
 }
