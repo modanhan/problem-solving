4b7737e6aaf180a3288dba105c08720b0b456183
shadowscat <modanhan@live.com>
2015-03-10 20:32:56 -0600

    need a gnu compiler...

--- psc/ccpc 2015/div1c.cpp
+++ psc/ccpc 2015/div1c.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 typedef long long ll;
 
+char hexc(int i){
+	if (i <= 9){
+		return i + 48;
+	}
+	else{
+		return i + 87;
+	}
+}
+
 int main() {
 	ll tc;
 	cin >> tc;
@@ -11,47 +20,56 @@ int main() {
 		cin >> n >> d;
 		vector<int> s(d, 0);
 		for (int i = 0; i < d; i++) {
-			char x;cin>>x;
-			if(x>='0'&&x<='9'){
-				s[i]=x-48;
-			}else{
-				s[i]=x-87;
+			char x; cin >> x;
+			if (x >= '0'&&x <= '9'){
+				s[i] = x - 48;
+			}
+			else{
+				s[i] = x - 87;
 			}
 		}
-		vector<ll> v(n, 0);
+		vector<bool> v(n, 0);
+		vector<string> ans(n, "");
 		queue<ll> q;
-		if (s[0] != 0) {
-			q.push(s[0]);
-			v[s[0]] = s[0];
-		} else if (s.size() >= 2) {
-			q.push(s[1]);
-			v[s[1]] = s[1];
-		} else {
+		if (s.size()==1&&s[0]==0) {
 			cout << "no solution\n";
 			continue;
+		}
+		else {
+			for (int i : s){
+				if (i != 0){
+					q.push(i);
+					v[i] = 1;
+					ans[i] = hexc(i);
+				}
+			}
 		}
 		while (!q.empty()) {
 			ll t = q.front();
 			q.pop();
-			if(t%n==0){
-				v[0]=t;
+			if (t%n == 0){
+				v[0] = t;
+				ans[0] = ans[t];
 				goto good;
 			}
-			for (ll nb : s) {
+			for (int nb : s) {
 				ll m = (t * 16 + nb) % n;
-				if(m==0){
-					v[0]=(t*16+nb);
+				if (m == 0){
+					ans[0] = ans[t] + hexc(nb);
 					goto good;
 				}
 				if (v[m] == 0) {
-					v[m] = t*16+nb;
-					q.push(t * 16 + nb);
+					ans[m] =ans[t]+ hexc(nb);
+					v[m] = 1;
+					q.push(m);
 				}
 			}
 		}
-		if(v[0]==0){
-		cout << "no solution\n";
-		}else{
-		good: cout<<hex << v[0] << "\n";}
+		if (v[0] == 0){
+			cout << "no solution\n";
+		}
+		else{
+		good: cout <<ans[0] << "\n";
+		}
 	}
 }
