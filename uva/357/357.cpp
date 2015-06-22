d2150c123d9b9a838caac626a045631a25022f7e
spoonsplz <modanhan@live.com>
2015-06-21 22:21:37 -0600

    ok i see what happened..

--- uva/357/357.cpp
+++ uva/357/357.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-<<<<<<< HEAD
 typedef long long ll;
 int main(){
 	ll coins[5]={1,5,10,25,50};
@@ -28,30 +27,4 @@ int main(){
 		}
 	}
 }
-=======
 
-typedef long long ll;
-
-int main() {
-	ll x, c[5] = { 1, 5, 10, 25, 50 };
-	vector<ll> dp(30001, 0);
-	dp[0] = 1;
-	for (int i = 0; i < 5; i++) {
-		for (int m = 0; m < 30001; m++) {
-			if (m >= c[i]) {
-				dp[m] += dp[m - c[i]];
-			}
-		}
-	}
-	while (cin >> x) {
-		if (dp[x] == 1) {
-			cout << "There is only 1 way to produce " << x
-					<< " cents change.\n";
-		} else {
-			cout << "There are " << dp[x] << " ways to produce " << x
-					<< " cents change.\n";
-		}
-
-	}
-}
->>>>>>> 43275328949b1b761e5a42f53c9de7c8c55229ea
